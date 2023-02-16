794c4c9049ffe118d34f1289b47cc0a76f00260f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-16 11:30:59 +0300

    Copied over some code from the App component

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -2,6 +2,42 @@ import React from 'react'
 
 const useHttp = () =>
 {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [tasks, setTasks] = useState([]);
+
+    const fetchTasks = async (taskText) =>
+    {
+        setIsLoading(true);
+        setError(null);
+        try
+        {
+            const response = await fetch(
+                'https://simple-react-backend-default-rtdb.firebaseio.com/tasks.json'
+            );
+
+            if (!response.ok)
+            {
+                throw new Error('Request failed!');
+            }
+
+            const data = await response.json();
+
+            const loadedTasks = [];
+
+            for (const taskKey in data)
+            {
+                loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+            }
+
+            setTasks(loadedTasks);
+        } catch (err)
+        {
+            setError(err.message || 'Something went wrong!');
+        }
+        setIsLoading(false);
+    };
+
     return (
         <div>useHttp</div>
     )
